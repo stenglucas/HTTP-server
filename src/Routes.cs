fe7c9e45e82d652b32ffb689f7f2ba474afb1b29
stenglucas <stenglucas@gmail.com>
2026-07-06 10:38:29 -0300

    Handle file GET

---
+++ src/Routes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_http_server.src
+{
+    internal class Routes
+    {
+        public const string Base = "/";
+        public const string Echo = "/echo";
+        public const string UserAgent = "/user-agent";
+        public const string Files = "/files";
+        
+    }
+}
---
+++ src/Server.cs
@@ -0,0 +1,37 @@
+﻿using codecrafters_http_server.src;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger Logger = factory.CreateLogger("Program");
+
+        string? Dir = null; 
+        if(args is not null && args.Length > 0 )
+        {
+            Logger.LogInformation($"Received {args.Length} args: {string.Join(", ", args)}");
+            if (args[0] == "--directory") 
+            {
+                if (args.Length != 2)
+                {
+                    Logger.LogError($"Received --directory arg, but args length wasn't 2; actual length: {args.Length}");
+                }
+                else
+                {
+                    Dir = args[1];
+                }
+            }
+        }
+
+        var Server = new HttpServer(IPAddress.Any, 4221, Logger, Dir);
+
+        Server.Start();
+        //Console.ReadLine();
+    }
+}
